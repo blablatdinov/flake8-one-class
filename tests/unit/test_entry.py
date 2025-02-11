168e3fe07aa57c55db656cf9a1e7d5dfe6c0247e
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-02-11 13:18:40 +0300

    Ruff

--- tests/unit/test_entry.py
+++ tests/unit/test_entry.py
@@ -20,31 +20,35 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 
+"""Unit test flake8-one-class plugin."""
+
 import ast
+from typing import Callable
 
 import pytest
 
 from flake8_one_class.entry import Plugin
 
+_PLUGIN_RUN_T = Callable[
+    [str],
+    list[tuple[int, int, str]],
+]
+
 
 @pytest.fixture
-def plugin_run():
+def plugin_run() -> _PLUGIN_RUN_T:
     """Fixture for easy run plugin."""
     def _plugin_run(code: str) -> list[tuple[int, int, str]]:  # noqa: WPS430
         """Plugin run result."""
         plugin = Plugin(ast.parse(code))
-        res = []
-        for viol in plugin.run():
-            res.append((
-                viol[0],
-                viol[1],
-                viol[2],
-            ))
-        return res
+        return [
+            (viol[0], viol[1], viol[2])
+            for viol in plugin.run()
+        ]
     return _plugin_run
 
 
-def test_wrong(plugin_run):
+def test_wrong(plugin_run: _PLUGIN_RUN_T) -> None:
     """Test wrong case."""
     got = plugin_run('\n'.join([
         'class Animal(object):',
@@ -63,7 +67,7 @@ def test_wrong(plugin_run):
     ]
 
 
-def test_valid(plugin_run):
+def test_valid(plugin_run: _PLUGIN_RUN_T) -> None:
     """Test valid case."""
     got = plugin_run('\n'.join([
         'class Animal(object):',
@@ -73,7 +77,7 @@ def test_valid(plugin_run):
     assert not got
 
 
-def test_with_private_class(plugin_run):
+def test_with_private_class(plugin_run: _PLUGIN_RUN_T) -> None:
     """Test valid case."""
     got = plugin_run('\n'.join([
         'class Animal(object):',
